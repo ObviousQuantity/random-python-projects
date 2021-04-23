a49decfbba3d7141f71cec1fdc0d70f178db5b83
ObviousQuantity <48537537+ObviousQuantity@users.noreply.github.com>
2021-04-23 09:28:17 -0400

    Python Tutorial: if __name__ == '__main__'

---
+++ Tutorials/OOP/__name__ == __main__/first_module.py
@@ -0,0 +1,7 @@
+print("This will always run")
+
+def main():
+    print(f"First module main method")
+
+if __name__ == '__main__':
+    main()
---
+++ Tutorials/OOP/__name__ == __main__/second_module.py
@@ -0,0 +1,5 @@
+import first_module
+
+first_module.main()
+
+print(f"Second module name: {__name__}")
